d517d5977c605013e3438e941c4720a709472d5a
Xie Yuheng <xyheme@gmail.com>
2019-06-20 17:05:18 +0800

    up

---
+++ src/main/scala/com/example/StreamQuickstart.scala
@@ -0,0 +1,22 @@
+package com.example
+
+import akka.stream.{ ActorMaterializer }
+import akka.stream.scaladsl._
+import akka.{ Done, NotUsed }
+import akka.actor.ActorSystem
+import akka.util.ByteString
+import scala.concurrent._
+import scala.concurrent.duration._
+import java.nio.file.Paths
+
+object StreamQuickstart extends App {
+  implicit val system = ActorSystem("QuickStart")
+  implicit val materializer = ActorMaterializer()
+
+  val source: Source[Int, NotUsed] = Source(1 to 100)
+
+  val done: Future[Done] = source.runForeach(i => println(i))
+
+  implicit val ec = system.dispatcher
+  done.onComplete(_ => system.terminate())
+}
