831a160cd1e4a47f24b40cde1a5380548e4b0039
Xie Yuheng <xyheme@gmail.com>
2019-07-01 15:27:01 +0800

    CollectingHeader

---
+++ src/main/scala/xieyuheng/cookbook/akka/CollectingHeader.scala
@@ -0,0 +1,26 @@
+package xieyuheng.cookbook.akka
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.headers.`Set-Cookie`
+import akka.http.scaladsl.model._
+import akka.stream.ActorMaterializer
+
+import scala.concurrent.ExecutionContextExecutor
+import scala.concurrent.Future
+
+object CollectingHeader extends App {
+  implicit val system: ActorSystem = ActorSystem()
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+
+  val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = "https://akka.io"))
+
+  responseFuture.map {
+    case response @ HttpResponse(StatusCodes.OK, _, _, _) =>
+      val setCookies = response.headers[`Set-Cookie`]
+      println(s"Cookies set by a server: $setCookies")
+      response.discardEntityBytes()
+    case _ => sys.error("something wrong")
+  }
+}
--- src/main/scala/xieyuheng/cookbook/akka/WebClientDida.scala
+++ src/main/scala/xieyuheng/cookbook/akka/WebClientDida.scala
@@ -12,61 +12,28 @@ import MediaTypes._
 import HttpCharsets._
 import HttpProtocols._
 
-object WebClientDida {
-  def main(args: Array[String]): Unit = {
-    implicit val system = ActorSystem()
-    implicit val materializer = ActorMaterializer()
-    // needed for the future flatMap/onComplete in the end
-    implicit val executionContext: ExecutionContext =
-      // ExecutionContext.global
-      system.dispatcher
-
-    val response: Future[HttpResponse] =
-      Http().singleRequest(HttpRequest(
-        POST,
-        uri = "http://api.didatravel.com/api/staticdata/GetCountryList?$format=json",
-        entity = HttpEntity(
-          `application/json`,
-          """{ "Header": { "LicenseKey": "DidaApiTestID", "ClientID": "TestKey" } }"""),
-        // protocol = `HTTP/1.1`
-      ))
-
-    response.onComplete {
-      case Success(res) => println(res.entity)
-      case Failure(_) => sys.error("something wrong")
-    }
-
-    response.foreach { res => println(res.entity) }
-  }
-}
-
-object MarshallerExample extends App {
-  import scala.concurrent.Await
-  import scala.concurrent.duration._
-  import akka.http.scaladsl.marshalling.Marshal
-  import akka.http.scaladsl.model._
-
+object WebClientDida extends App {
   implicit val system = ActorSystem()
-  import system.dispatcher // ExecutionContext
+  implicit val materializer = ActorMaterializer()
+  // needed for the future flatMap/onComplete in the end
+  implicit val executionContext: ExecutionContext =
+    // ExecutionContext.global
+    system.dispatcher
 
-  val string = "Yeah"
-  val entityFuture = Marshal(string).to[MessageEntity]
-  val entity = Await.result(entityFuture, 1.second) // don't block in non-test code!
-  println(entity)
-  // entity.contentType shouldEqual ContentTypes.`text/plain(UTF-8)`
+  val response: Future[HttpResponse] =
+    Http().singleRequest(HttpRequest(
+      POST,
+      uri = "http://api.didatravel.com/api/staticdata/GetCountryList?$format=json",
+      entity = HttpEntity(
+        `application/json`,
+        """{ "Header": { "LicenseKey": "DidaApiTestID", "ClientID": "TestKey" } }"""),
+      // protocol = `HTTP/1.1`
+    ))
 
-  val errorMsg = "Easy, pal!"
-  val responseFuture = Marshal(420 -> errorMsg).to[HttpResponse]
-  val response = Await.result(responseFuture, 1.second) // don't block in non-test code!
-  println(response)
-  // response.status shouldEqual StatusCodes.EnhanceYourCalm
-  // response.entity.contentType shouldEqual ContentTypes.`text/plain(UTF-8)`
+  response.onComplete {
+    case Success(res) => println(res.entity)
+    case Failure(_) => sys.error("something wrong")
+  }
 
-  val request = HttpRequest(headers = List(headers.Accept(MediaTypes.`application/json`)))
-  val responseText = "Plaintext"
-  val respFuture = Marshal(responseText).toResponseFor(request) // with content negotiation!
-  println(respFuture)
-  // a[Marshal.UnacceptableResponseContentTypeException] should be thrownBy {
-  //   Await.result(respFuture, 1.second) // client requested JSON, we only have text/plain!
-  // }
+  response.foreach { res => println(res.entity) }
 }
