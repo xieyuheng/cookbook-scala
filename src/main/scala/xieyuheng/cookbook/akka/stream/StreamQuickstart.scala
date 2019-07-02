3ced5f8937590e483685691964946fae927de4d5
Xie Yuheng <xyheme@gmail.com>
2019-07-02 17:52:42 +0800

    up

--- src/main/scala/xieyuheng/cookbook/akka/stream/StreamQuickstart.scala
+++ src/main/scala/xieyuheng/cookbook/akka/stream/StreamQuickstart.scala
@@ -1,22 +1,38 @@
-package xieyuheng.cookbook.akka
+package xieyuheng.cookbook.akka.stream
 
 import akka.stream.{ ActorMaterializer }
-import akka.stream.scaladsl._
+import akka.stream.scaladsl.{ Source, Sink, Flow, Keep, RunnableGraph }
 import akka.{ Done, NotUsed }
 import akka.actor.ActorSystem
 import akka.util.ByteString
 import scala.concurrent._
 import scala.concurrent.duration._
+import scala.util.{ Failure, Success }
 import java.nio.file.Paths
 
 object StreamQuickstart extends App {
-  implicit val system = ActorSystem("QuickStart")
-  implicit val materializer = ActorMaterializer()
-
   val source: Source[Int, NotUsed] = Source(1 to 100)
+  val flow: Flow[Int, Int, NotUsed] = Flow[Int].map(2 * _)
+  val sink: Sink[Int, Future[Int]] = Sink.fold(0)(_ + _)
+  val runnable: RunnableGraph[Future[Int]] =
+    source
+      .async.via(flow)
+      .toMat(sink)(Keep.right)
 
-  val done: Future[Done] = source.runForeach(i => println(i))
+  implicit val system = ActorSystem("StreamQuickstart")
+  implicit val materializer = ActorMaterializer()
+  implicit val exeCtx = system.dispatcher
 
-  implicit val ec = system.dispatcher
-  done.onComplete(_ => system.terminate())
+  val sum: Future[Int] = runnable.run()
+
+  sum.onComplete {
+    case Success(int) => {
+      println(int)
+      system.terminate()
+    }
+    case Failure(err) => {
+      println(err)
+      system.terminate()
+    }
+  }
 }
