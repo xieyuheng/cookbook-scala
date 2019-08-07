534295ec28a1fa726335f20067dba6a17fae3892
Xie Yuheng <xyheme@gmail.com>
2019-08-07 16:28:13 +0800

    up

--- src/main/scala/xieyuheng/cookbook/akka/http/QuickStart.scala
+++ src/main/scala/xieyuheng/cookbook/akka/http/QuickStart.scala
@@ -6,41 +6,64 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.marshalling.Marshal
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 
+
+import scala.concurrent.{ ExecutionContext, Future }
 import scala.io.StdIn
+
+/**
+  * https://richardimaoka.github.io/blog/akka-http-quickstart
+  */
+
+case class User(
+  familyName: String,
+  givenName: String,
+  age: Int)
+
+object User {
+  import spray.json.DefaultJsonProtocol._
+  import spray.json.RootJsonFormat
+
+  implicit val userJsonFormat: RootJsonFormat[User] =
+    jsonFormat3(User.apply)
+}
 
 object QuickStartApp extends App {
   implicit val system = ActorSystem("QuickStart")
   implicit val materializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
 
-  val route = pathPrefix("user") {
-    path("hello") {
+  val route: Route = pathPrefix("user") {
+    path("xie") {
       get {
-        complete(HttpEntity(
-          ContentTypes.`text/html(UTF-8)`,
-          "<h1>Say hello to akka-http</h1>"))
+        complete(User("xie", "yuheng", 400))
       }
-    } ~ path("hi") {
+    } ~ path("yu") {
       get {
-        complete(HttpEntity(
-          ContentTypes.`text/html(UTF-8)`,
-          "hi"))
+        complete(User("yu", "hengxie", 300))
+      }
+    } ~ path("heng") {
+      get {
+        complete(User("heng", "yuxie", 200))
       }
     }
   }
 
   val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
-
   println(s"Server online at http://localhost:8080/")
-  // println(s"Press RETURN to stop...")
-  // StdIn.readLine() // let it run until user presses return
-  // bindingFuture
-  //   // .flatMap(_.unbind()) // trigger unbinding from the port
-  //   .flatMap(x => {
-  //     println(x)
-  //     x.unbind()
-  //   }) // trigger unbinding from the port
-  //   .onComplete(_ => system.terminate()) // and shutdown when done
+  println(s"Press RETURN to stop...")
+  StdIn.readLine() // let it run until user presses return
 
+  bindingFuture
+    .flatMap(binding => {
+      // trigger unbinding from the port
+      println(s"unbind: ${binding}")
+      binding.unbind()
+    })
+    .onComplete(result => {
+      println(s"result: ${result}")
+      system.terminate()
+    })
 }
